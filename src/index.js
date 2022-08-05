3047792993c03ffc1a83cd5d8c3ba896f9472500
kiqueangulo <kiqueangulo@gmail.com>
2022-08-05 11:59:38 -0400

    Setting up ContextProvider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { ContextProvider } from "./context/ContextProvider";
+
 import App from "./App";
 
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  document.getElementById("root")
+);
