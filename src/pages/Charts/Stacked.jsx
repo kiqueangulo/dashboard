4cdc14b7eee74a65cdb6af65d803dd2e7bb1c3dd
kiqueangulo <kiqueangulo@gmail.com>
2022-08-07 18:49:19 -0400

    Complete zoomed stacked chart

--- src/pages/Charts/Stacked.jsx
+++ src/pages/Charts/Stacked.jsx
@@ -5,6 +5,9 @@ const Stacked = () => {
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
       <ChartsHeader category="Stacked" title="Revenue Breakdown" />
+      <div className="w-full">
+        <StackedChart />
+      </div>
     </div>
   );
 };
