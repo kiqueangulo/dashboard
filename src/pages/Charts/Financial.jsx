3f9c2358400eb6aef618076d73a1975457c4552f
kiqueangulo <kiqueangulo@gmail.com>
2022-08-07 18:24:47 -0400

    Complete color mapping chart

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -31,7 +31,7 @@ function filterValue(value) {
 const returnValue = financialChartData.filter(filterValue);
 
 const Financial = () => {
-  const { currentMode, activeMenu } = useStateContext();
+  const { currentMode } = useStateContext();
 
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
