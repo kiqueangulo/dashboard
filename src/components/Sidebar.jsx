f9e7cf5650b971cc66ba56bd94787947459e2109
kiqueangulo <kiqueangulo@gmail.com>
2022-08-06 23:06:02 -0400

    Set dark mode up
    There a couple of things left to implement dark mode

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -38,7 +38,7 @@ const Sidebar = () => {
                 onClick={() =>
                   setActiveMenu((prevActiveMenu) => !prevActiveMenu)
                 }
-                className="text-xl rounded-br-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
               >
                 <MdOutlineCancel />
               </button>
