d20325bee470e1b610c579175dae3502f8d4e820
kiqueangulo <kiqueangulo@gmail.com>
2022-08-06 21:15:20 -0400

    Set up themes component

--- src/App.js
+++ src/App.js
@@ -64,6 +64,8 @@ const App = () => {
             </div>
 
             <div className="component-visible">
+              <ThemeSettings />
+
               <Routes>
                 {/* Dashboard */}
                 <Route path="/" element={<Ecommerce />} />
